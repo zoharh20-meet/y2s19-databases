05b16c772533e3bb46fbbfd21684577efa9fabcd
myname21-meet <myname21@meet.mit.edu>
2019-07-29 03:33:27 -0400

    database lab

--- knowledge_model.py
+++ knowledge_model.py
@@ -14,4 +14,28 @@ class Knowledge(Base):
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
 
-	pass
+		__tablename__ = 'English Rock Bands'
+		article_id = Column(Integer, primary_key=True)
+		title = Column(String)
+		topic = Column(String)
+		rating = Column(Integer)
+
+		def __repr__(self):
+			 return ("If you want to learn about {},"
+			 		 "you should look at the Wikipedia article called {}. "
+					"We gave this article a rating of {} out of 10!").format(self.topic, self.title, self.rating)
+		    return ("Title: {}\n"
+	               "Topic: {} \n"
+	               "Rating: {}").format(
+	                    self.title,
+	                    self.topic,
+	                    self.rating)
+
+article1 = Knowledge(title = "King Crimson",  topic= "An Enslish rock-band" , rating= 10) 
+print(article1)
+
+article2 = Knowledge(title="Led Zeppelin", topic= "Another English rock-band", rating= 9)
+print(article2)
+
+article3 = Knowledge(title=  "Black Sbbath", topic= "Wow, Another English rock-band :|", rating= 8)
+print(article3)
